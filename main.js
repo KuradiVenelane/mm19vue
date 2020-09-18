205714d5f92f2cc8e96ea74f81accc0bf411c0ee
Tehnikumi Opilane <opilane@tptlive.ee>
2020-09-18 11:28:38 +0300

    init commit

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 import Vue from "vue/dist/vue.common.js"
 
-import Message from "./Message.vue";
-Vue.component("message", Message)
+import todo from "./todo.vue";
+import modelexample from "./modelexample.vue";
+import TabsExample from "./TabsExample.vue";
+import JsonExample from "./Jsonexample.vue";
+import vueCommon from "vue/dist/vue.common.js";
+Vue.component("to-do", todo);
+vue.component("model-example", modelexample);
+vue.component("tabs-example",TabsExample)
 
 let app = new Vue ({
     el: "#app",
