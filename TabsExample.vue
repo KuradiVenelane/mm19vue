c31cf6cf3d2d26a4cbac0f3725f9f2b8ca0baceb
Tehnikumi Opilane <opilane@tptlive.ee>
2020-09-18 11:05:58 +0300

    init commit

---
+++ TabsExample.vue
@@ -0,0 +1,16 @@
+<template>
+    <tabs></tabs>
+</template>
+<script>
+import tabs from './tabs.vue';
+export default {
+    components: {
+        tabs: tabs
+    },
+    data() {
+        return {
+            headers: ["games","books","shows","music"]
+        }
+    }
+})
+</script>
---
+++ tabs.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="tabs">
+        <ul>
+            <li v-for="(title,index) in titles" 
+            :key="index"
+            :class="{"is-active": index == active}"
+            @click="active=index"
+            <a>{{title}}</a></li>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            titles: ["games","books","shows","music"]
+            active: 0
+        }
+    }
+}
+</script>
